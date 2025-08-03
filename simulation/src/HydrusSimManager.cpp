14b82c26c6216fdfcc1e11ece2d22e531bdee22f
JuanDelPueblo <edyancruz@outlook.com>
2025-08-03 17:58:58 -0400

    Add working hydrus model

--- simulation/src/HydrusSimManager.cpp
+++ simulation/src/HydrusSimManager.cpp
@@ -1,6 +1,8 @@
 #include "HydrusSimManager.h"
 #include <Stonefish/entities/statics/Plane.h>
 #include <Stonefish/entities/solids/Sphere.h>
+#include <Stonefish/entities/solids/Polyhedron.h>
+#include <utils/SystemUtil.hpp>
 #include <core/NED.h>
 
 HydrusSimManager::HydrusSimManager(sf::Scalar stepsPerSecond) : SimulationManager(stepsPerSecond)
@@ -9,7 +11,7 @@ HydrusSimManager::HydrusSimManager(sf::Scalar stepsPerSecond) : SimulationManage
 
 void HydrusSimManager::BuildScenario()
 {
-    // Physical materials
+    // Physical materialsw
     CreateMaterial("Aluminium", 2700.0, 0.8);
     CreateMaterial("Steel", 7810.0, 0.9);
     SetMaterialsInteraction("Aluminium", "Aluminium", 0.7, 0.5);
@@ -17,6 +19,7 @@ void HydrusSimManager::BuildScenario()
     SetMaterialsInteraction("Aluminium", "Steel", 0.6, 0.4);
 
     // Graphical materials (looks)
+    CreateLook("black", sf::Color::RGB(0.f, 0.f, 0.f), 0.1f, 0.1f);
     CreateLook("gray", sf::Color::Gray(0.5f), 0.3f, 0.2f);
     CreateLook("red", sf::Color::RGB(1.f, 0.f, 0.f), 0.1f, 0.f);
 
@@ -25,16 +28,19 @@ void HydrusSimManager::BuildScenario()
     getOcean()->setWaterType(0.2);
     getOcean()->EnableCurrents();
     getAtmosphere()->SetSunPosition(0.0, 60.0);
-    getNED()->Init(41.77737, 3.03376, 0.0);
+    getNED()->Init(0.0, 0.0, 0.0);
 
     sf::Plane *plane = new sf::Plane("Ground", 10000.0, "Steel", "gray");
-    AddStaticEntity(plane, sf::I4());
-
-    // Set object physics settings
-    sf::BodyPhysicsSettings phy;
-    phy.mode = sf::BodyPhysicsMode::SUBMERGED;
+    AddStaticEntity(plane, sf::Transform(sf::IQ(), sf::Vector3(0.0, 0.0, 5.0)));
 
     // Create object
+    sf::BodyPhysicsSettings phy;
+    phy.mode = sf::BodyPhysicsMode::SUBMERGED;
+    phy.collisions = false;
+
     sf::Sphere *sph = new sf::Sphere("Sphere", phy, 0.1, sf::I4(), "Aluminium", "red");
     AddSolidEntity(sph, sf::Transform(sf::IQ(), sf::Vector3(0.0, 0.0, -1.0)));
+
+    sf::Polyhedron *poly = new sf::Polyhedron("Hydrus", phy, sf::GetDataPath() + "models/hydrus_unibody.obj", sf::Scalar(0.01), sf::I4(), "Steel", "black");
+    AddSolidEntity(poly, sf::Transform(sf::IQ(), sf::Vector3(0.0, -1.0, 0.0)));
 }
--- simulation/src/main.cpp
+++ simulation/src/main.cpp
@@ -1,14 +1,23 @@
 #include <Stonefish/core/GraphicalSimulationApp.h>
 #include "HydrusSimManager.h"
+#include <filesystem>
+#include <string>
 
 int main(int argc, char **argv)
 {
     // Using default settings
     sf::RenderSettings s;
+    s.verticalSync = true; // Enable vertical sync
+    s.windowH = 1024;
+    s.windowW = 1280; // Set window size
     sf::HelperSettings h;
 
+    // Get the directory where the executable's source is located
+    std::filesystem::path sourcePath = std::filesystem::path(__FILE__).parent_path().parent_path();
+    std::string simulationPath = sourcePath.string() + "/";
+
     HydrusSimManager manager(500.0);
-    sf::GraphicalSimulationApp app("Simple simulator", "path_to_data", s, h, &manager);
+    sf::GraphicalSimulationApp app("Simple simulator", simulationPath, s, h, &manager);
     app.Run();
 
     return 0;
