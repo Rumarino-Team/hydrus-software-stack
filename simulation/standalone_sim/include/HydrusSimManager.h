575cbcf7b7e6fa5d7bf3662f214077b450ae227a
JuanDelPueblo <edyancruz@outlook.com>
2025-09-18 09:27:17 -0400

    Remove standalone sim

--- simulation/standalone_sim/include/HydrusSimManager.h
+++
@@ -1,8 +0,0 @@
-#include <Stonefish/core/SimulationManager.h>
-
-class HydrusSimManager : public sf::SimulationManager
-{
-public:
-    HydrusSimManager(sf::Scalar stepsPerSecond);
-    void BuildScenario();
-};
